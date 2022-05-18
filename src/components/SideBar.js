4b1aca0270d87d7f586812b29d8a49a9d2f035f3
Vishnu-Aithal <90306959+Vishnu-Aithal@users.noreply.github.com>
2022-05-19 00:34:52 +0530

    feat - process - tags

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,5 +1,5 @@
 import { NavLink } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
     PencilIcon,
     ArchiveIcon,
@@ -7,6 +7,7 @@ import {
     TrashIcon,
     TagIcon,
 } from "components/Icons.js";
+import { useSelector } from "react-redux";
 
 const SideBarNavLink = ({ to, Icon, name }) => {
     return (
@@ -39,15 +40,15 @@ const TagSubMenu = ({ tags }) => {
                 <SideBarNavLink to={"/tags"} Icon={TagIcon} name="Tags" />
             </div>
             <ul
-                className={`sm:ml-2 ml-1 overflow-hidden transition-all text-xs ${
+                className={`sm:ml-2 ml-1 overflow-hidden transition-all text-xs space-y-1 ${
                     subMenuCollapse ? "h-0" : "h-0 sm:h-auto"
                 }`}>
-                {tags.map((tag, index) => (
-                    <li key={index}>
+                {tags.map((tag) => (
+                    <li className="mt-1" key={tag}>
                         <SideBarNavLink
-                            to={"/tags/" + tag.id}
+                            to={`/tags/${tag}`}
                             Icon={TagIcon}
-                            name={tag.name}
+                            name={tag.toUpperCase()}
                         />
                     </li>
                 ))}
@@ -57,6 +58,20 @@ const TagSubMenu = ({ tags }) => {
 };
 
 export const SideBar = () => {
+    const [tags, setTags] = useState([]);
+    const notes = useSelector((state) => state.allNotes.notes);
+
+    useEffect(() => {
+        const getTags = () => {
+            let tags = notes.reduce(
+                (tags, note) => [...tags, ...note.tags],
+                []
+            );
+            tags = Array.from(new Set(tags));
+            return ["ALL", ...tags];
+        };
+        setTags(getTags(notes));
+    }, [notes, setTags]);
     return (
         <aside className="h-full w-fit overflow-auto dark:bg-zinc-800 dark:text-slate-300">
             <ul className="py-2 text-sm font-medium space-y-1">
@@ -77,14 +92,7 @@ export const SideBar = () => {
                 </li>
 
                 <li>
-                    <TagSubMenu
-                        tags={[
-                            { id: 0, name: "tag1" },
-                            { id: 1, name: "tag2" },
-                            { id: 2, name: "tag3" },
-                            { id: 3, name: "tag4" },
-                        ]}
-                    />
+                    <TagSubMenu tags={tags} />
                 </li>
 
                 <li>
