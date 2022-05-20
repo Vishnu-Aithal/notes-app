6c81e925b7c1a866cae2c1927e349acdafc90d91
Vishnu-Aithal <90306959+Vishnu-Aithal@users.noreply.github.com>
2022-05-21 00:52:41 +0530

    fix - remove bottom scrollbar

--- src/components/ContentLayout.js
+++ src/components/ContentLayout.js
@@ -1,6 +1,6 @@
 export const ContentLayout = ({ children }) => {
     return (
-        <div className="content w-full px-1 py-5 sm:px-5 flex flex-wrap gap-6 content-start overflow-scroll dark:bg-zinc-800 sm:justify-start justify-center animate-fade-in">
+        <div className="content w-full px-1 py-5 sm:px-5 flex flex-wrap gap-6 content-start overflow-y-scroll dark:bg-zinc-800 sm:justify-start justify-center animate-fade-in">
             {children}
         </div>
     );
