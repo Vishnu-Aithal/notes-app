f2a5b251eb46d5a56fd1bd3eb0d64617ef302ee2
Vishnu-Aithal <90306959+Vishnu-Aithal@users.noreply.github.com>
2022-05-21 00:35:02 +0530

    change - render notes from filterslice

--- src/pages/ArchivesPage.js
+++ src/pages/ArchivesPage.js
@@ -1,12 +1,21 @@
 import { ContentLayout } from "components/ContentLayout";
+import { Filter } from "components/Filter";
 import { Note } from "components/Note";
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setUnFilteredNotes } from "store/filteredNotesSlice";
 
 export const ArchivesPage = () => {
     const archives = useSelector((state) => state.allNotes.archives);
+    const dispatch = useDispatch();
+    const { filteredNotes } = useSelector((state) => state.filteredNotes);
+    useEffect(() => {
+        dispatch(setUnFilteredNotes(archives));
+    }, [archives, dispatch]);
     return (
         <ContentLayout>
-            {archives.map((note) => (
+            <Filter />
+            {filteredNotes.map((note) => (
                 <Note key={note._id} note={note} />
             ))}
         </ContentLayout>
--- src/pages/TrashPage.js
+++ src/pages/TrashPage.js
@@ -1,12 +1,21 @@
 import { ContentLayout } from "components/ContentLayout";
+import { Filter } from "components/Filter";
 import { Note } from "components/Note";
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setUnFilteredNotes } from "store/filteredNotesSlice";
 
 export const TrashPage = () => {
     const trash = useSelector((state) => state.allNotes.trash);
+    const dispatch = useDispatch();
+    const { filteredNotes } = useSelector((state) => state.filteredNotes);
+    useEffect(() => {
+        dispatch(setUnFilteredNotes(trash));
+    }, [trash, dispatch]);
     return (
         <ContentLayout>
-            {trash.map((note) => (
+            <Filter />
+            {filteredNotes.map((note) => (
                 <Note key={note._id} note={note} />
             ))}
         </ContentLayout>
